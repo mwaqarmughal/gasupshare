0daebe2fb10ddc828d23b231a9cc3bdc43728db9
Muhammad Waqar Mughal <56934353+mwaqarmughal@users.noreply.github.com>
2021-05-13 02:23:32 +0500

    Update partnership.js

--- routes/partnership.js
+++ routes/partnership.js
@@ -9,7 +9,6 @@ router.get("/", async (req, res) => {
   try {
     const partnership = await Partnership.find();
     res.send(partnership);
-    console.log("good to go")
   } catch (error) {
     res.json({message: error})
   };
